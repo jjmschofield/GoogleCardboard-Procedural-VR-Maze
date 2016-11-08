241d211c94fd959e22dbd403d4ca0d668f12bf10
jjmschofield <jjmschofield@googlemail.com>
2016-11-08 13:54:01 +0000

    Adding the start of the maze cell

---
+++ Assets/Scripts/Maze/MazeCell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProceduralMaze { 
+    public class MazeCell {
+        public PositionalGraphNode mazeGrpahNode;
+        public PositionalGraphNode[] wallGraphNodes; //0 = top left, 1 = top right, 2  = bottom right, 3 = bottom left       
+
+        public MazeCell(PositionalGraphNode mazeGrpahNode)
+        {
+            this.mazeGrpahNode = mazeGrpahNode;
+            wallGraphNodes = new PositionalGraphNode[4]; //We assum a square maze
+
+        }
+
+    }
+}
