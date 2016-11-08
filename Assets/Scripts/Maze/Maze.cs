3d23506bb5885e87dd0521c3bef3e159efbfa6b3
jjmschofield <jjmschofield@googlemail.com>
2016-11-08 13:54:31 +0000

    Adding basic math for determining corners

--- Assets/Scripts/Maze/Maze.cs
+++ Assets/Scripts/Maze/Maze.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 namespace ProceduralMaze
@@ -10,7 +11,8 @@ namespace ProceduralMaze
         public int width = 20;
         public int height = 20;
         public PositionalGraph mazeNodeGraph;
-        public PositionalGraph mazeWallGraph;               
+        public PositionalGraph mazeWallGraph;
+        public List<MazeCell> mazeCells;               
         
         void Start()
         {
@@ -18,6 +20,28 @@ namespace ProceduralMaze
                         
             mazeWallGraph = new PositionalGraph(width + 1 , height + 1);
             mazeWallGraph.ConnectNeighbourNodes();
+
+            List<PositionalGraphNode> mazeNodes = mazeNodeGraph.GetNodes();
+            List<PositionalGraphNode> wallNodes = mazeWallGraph.GetNodes();
+
+
+            for (int y = 0; y < width; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    MazeCell mazeCell = new MazeCell(mazeNodes[x + y]);
+
+                    Debug.Log("Cell " + (x + (y * width)));
+
+                    Debug.Log("TL " + (x + (y * width) + y));
+                    Debug.Log("TR " + (x + (y * width) + y + 1));
+                    Debug.Log("BL " + (x + (y * width) + width + y + 1));
+                    Debug.Log("BR " + (x + (y * width) + width + y + 2));
+
+                }
+            }
+
+
         }
 
         void Update()
