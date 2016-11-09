9e217a16bc6f9832b1d26c659ffa913e3c26fd02
jjmschofield <jjmschofield@googlemail.com>
2016-11-09 04:09:07 +0000

    Removing the concept of being in a square maze from a maze cell

--- Assets/Scripts/Maze/MazeCell.cs
+++ Assets/Scripts/Maze/MazeCell.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 namespace ProceduralMaze { 
     public class MazeCell {
         public PositionalGraphNode mazeGrpahNode;
-        public PositionalGraphNode[] wallGraphNodes; //0 = top left, 1 = top right, 2  = bottom right, 3 = bottom left       
+        public List<PositionalGraphNode> wallGraphNodes;
 
         public MazeCell(PositionalGraphNode mazeGrpahNode)
         {
             this.mazeGrpahNode = mazeGrpahNode;
-            wallGraphNodes = new PositionalGraphNode[4]; //We assum a square maze
+            wallGraphNodes = new List<PositionalGraphNode>();
 
         }
 
