65203db48585a0c7eb44403ef5ad2e00bd972ba5
Jack Schofield <jack.schofield@miicard.com>
2016-11-14 13:22:34 +0000

    Generating seperate objects for each child mesh and adding collision

--- Assets/Scripts/Maze/MazeMeshGenerator.cs
+++ Assets/Scripts/Maze/MazeMeshGenerator.cs
@@ -3,72 +3,111 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace ProceduralMaze
-{
-    [RequireComponent(typeof(MeshFilter))]
+{    
     public class MazeMeshGenerator : MonoBehaviour
     {
+        public Material wallMaterial;
+        public Material floorMaterial;
+        public Material ceilingMaterial;
+
+        public float wallHeight = 2.0F;
+        public float wallWidth = 0.5F;
+
+        GameObject wallObject;
+        GameObject floorObject;
+        GameObject ceilingObject;
+
         List<MazeCell> cells;
         PositionalGraph floorGraph;
-        PositionalGraph wallGraph;
-        MeshFilter meshFilter;
+        PositionalGraph wallGraph;        
         
         enum DIRECTION { horizontal, vertical };
 
-        void Start()
-        {
-            meshFilter = gameObject.GetComponent<MeshFilter>();
-        }
-
         public void UpdateMesh(List<MazeCell> cells, PositionalGraph floorGraph, PositionalGraph wallGraph)
-        {       
+        {           
+            CreateMeshObjects();
+
             this.cells = cells;
             this.floorGraph = floorGraph;
             this.wallGraph = wallGraph;
-                    
-            Mesh wallMesh = GenerateWallMesh();
-            Mesh floorMesh = GenerateFloorMesh();
 
-            //Generate floor
-            //Generate ceiling
-
-            meshFilter.mesh = floorMesh;
+            AssignMesh(floorObject, GenerateFloorMesh());
+            AssignMesh(wallObject, GenerateWallMesh());
+            AssignMesh(ceilingObject, GenerateCeilingMesh());                    
         }        
+
+        void DestroyExistingMeshObjects()
+        {
+            GameObject[] meshObjects = new GameObject[] { wallObject, floorObject, ceilingObject };
+
+            foreach (GameObject meshObject in meshObjects)
+            {
+                if (meshObject != null) GameObject.Destroy(meshObject);
+            }
+        }
+
+        void CreateMeshObjects()
+        {
+            DestroyExistingMeshObjects();
+            floorObject = CreateMeshObject("floor",floorMaterial);
+            wallObject = CreateMeshObject("wall", wallMaterial);
+            ceilingObject = CreateMeshObject("ceiling", ceilingMaterial);
+
+        }
+
+        GameObject CreateMeshObject(string name, Material material)
+        {
+            GameObject meshObject = new GameObject(name);
+            meshObject.transform.position = transform.position;
+            meshObject.transform.parent = transform;
+            meshObject.AddComponent<MeshFilter>();
+            meshObject.AddComponent<MeshRenderer>();
+            meshObject.AddComponent<MeshCollider>();
+            meshObject.GetComponent<MeshRenderer>().material = material;
+            return meshObject;
+        }
+
+        void AssignMesh(GameObject meshObject, Mesh mesh)
+        {
+            meshObject.GetComponent<MeshFilter>().mesh = mesh;
+            meshObject.GetComponent<MeshCollider>().sharedMesh = mesh;
+        }
 
          Mesh GenerateWallMesh()
         {
-            List<Cube> cubes = GetWallCubes();
+            List<Cube> cubes = GeenerateWallCubes();
             return new StitchedCubeMesh(cubes).mesh;
         }
 
-        List<Cube> GetWallCubes()
+        List<Cube> GeenerateWallCubes()
         {
             List<Cube> cubes = new List<Cube>();
-
-            float wallHeight = 2.0F;
-            float wallWidth = 0.5F;
+           
             foreach (GraphConnection<PositionalGraphNode> connection in wallGraph.GetConnections())
             {
-                DIRECTION wallDirection = GetWallDirection(connection);
-
-                Vector3 position = (connection.nodeA.position + connection.nodeB.position) / 2;
-                position.y += wallHeight / 2;
-                Vector3 vectorDistance = connection.nodeA.position - connection.nodeB.position;
-                float distance = vectorDistance.magnitude;
-
-                if (wallDirection == DIRECTION.horizontal)
-                {
-                    Cube cube = new Cube(position, distance, wallWidth, wallHeight);
-                    cubes.Add(cube);
-                }
-                else
-                {
-                    Cube cube = new Cube(position, wallWidth, distance, wallHeight);
-                    cubes.Add(cube);
-                }   
+                cubes.Add(GenerateWallCube(connection));
             }
-
             return cubes;
-        }        
+        }
+
+        Cube GenerateWallCube(GraphConnection<PositionalGraphNode> connection)
+        {
+            DIRECTION wallDirection = GetWallDirection(connection);
+
+            Vector3 position = (connection.nodeA.position + connection.nodeB.position) / 2;
+            position.y += wallHeight / 2;
+            Vector3 vectorDistance = connection.nodeA.position - connection.nodeB.position;
+            float distance = vectorDistance.magnitude;
+
+            if (wallDirection == DIRECTION.horizontal)
+            {
+                return new Cube(position, distance, wallWidth, wallHeight);             
+            }
+            else
+            {
+                return new Cube(position, wallWidth, distance, wallHeight);               
+            }            
+        }
 
         Mesh GenerateFloorMesh()
         {
@@ -76,14 +115,20 @@ namespace ProceduralMaze
             return new StitchedQuadMesh(quads).mesh;
         }
 
+        Mesh GenerateCeilingMesh()
+        {
+            List<Quad> quads = GeneratePlane(new Vector3(0,wallHeight,0), WINDING.anti_clockwise);
+            return new StitchedQuadMesh(quads).mesh;
+        }
 
-        List<Quad> GeneratePlane()
+
+        List<Quad> GeneratePlane(Vector3 positionOffset = new Vector3(), WINDING winding = WINDING.clockwise)
         {
             List<Quad> quads = new List<Quad>();
 
             foreach(PositionalGraphNode node in floorGraph.GetNodes())
             {
-                Quad quad = new Quad(node.position, 2, 2, WINDING.clockwise, Vector3.up);
+                Quad quad = new Quad(node.position + positionOffset, 2, 2, winding, Vector3.up);
                 quads.Add(quad);
             }
 
