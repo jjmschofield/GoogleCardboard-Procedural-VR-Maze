26d86784b684770b852913854630d30d53c172f1
Jack Schofield <jack.schofield@miicard.com>
2016-11-15 15:23:25 +0000

    Adding start of player navigation

---
+++ Assets/Scripts/Navigation/PlayerWaypoint.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class PlayerWaypoint : MonoBehaviour {
+
+    public Material defaultMaterial;
+    public Material highlightedMaterial;
+    public Material visitedMaterial;
+
+    bool isOccupied = false;
+    bool hasBeenVisitied = false;
+    bool isHighlighted = false;
+
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (!hasBeenVisitied)
+        {
+            meshRenderer.material = defaultMaterial;
+        }
+
+        if (hasBeenVisitied)
+        {
+            meshRenderer.material = visitedMaterial;
+        }
+
+
+        if (isHighlighted)
+        {
+            meshRenderer.material = highlightedMaterial;
+        }
+
+        isHighlighted = false;
+    }   
+
+    
+    public bool CanBeMovedToo()
+    {
+        return !isOccupied;
+    }
+
+    public bool HasBeenVisitied()
+    {
+        return hasBeenVisitied;
+    }
+
+    public void MoveTo()
+    {
+        isOccupied = true;
+        hasBeenVisitied = true;
+    }
+
+    public void Highlight()
+    {        
+        isHighlighted = true;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerWaypointMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWaypointMovement : MonoBehaviour {
+
+    public bool showDebugRays = false;
+    public LayerMask layerMask;
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateGazeSelect();
+	}
+
+    void UpdateGazeSelect()
+    {
+        PlayerWaypoint waypoint = CastForWaypoint();
+
+        if(waypoint != null && waypoint.CanBeMovedToo())
+        {
+            waypoint.Highlight();
+        }
+
+    }
+
+    PlayerWaypoint CastForWaypoint()
+    {
+        PlayerWaypoint waypoint = null;
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;             
+
+        if (Physics.Raycast(ray, out hit, 100F, layerMask))
+        {
+            waypoint = hit.transform.gameObject.GetComponent<PlayerWaypoint>();
+
+            if(waypoint != null)
+            {
+                if (showDebugRays) Debug.DrawLine(ray.origin, hit.point, Color.green);
+            }
+            else
+            {
+                if (showDebugRays) Debug.DrawLine(ray.origin, hit.point, Color.red);
+            }           
+            
+        }       
+     
+        return waypoint;
+    }
+
+
+
+
+}
